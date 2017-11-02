096512e12eb9d6e6f3cdc1211364d7105b349218
Lucas de Freitas Kauer <lucas.kauerrs@hotmail.com>
2017-11-02 15:17:38 -0200

    feat(HOME): add initial structure to a home page (after login)

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import './home.css';
+
 class Home extends React.Component {
 
   render() {
     return (
-      <main>
+      <main className='home'>
+        <h4>Mussum Ipsum, cacilds vidis litro abertis</h4>
+        <h4>Mussum Ipsum, cacilds vidis litro abertis</h4>
+        <h4>Mussum Ipsum, cacilds vidis litro abertis</h4>
+        <h4>Mussum Ipsum, cacilds vidis litro abertis</h4>
+        <h4>Mussum Ipsum, cacilds vidis litro abertis</h4>
         {this.props.children}
       </main>
     );

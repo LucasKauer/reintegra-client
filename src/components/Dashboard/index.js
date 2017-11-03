4674638e1cad7f7c1d56c4513a07f0da30a80ec5
Lucas de Freitas Kauer <lucas.kauerrs@hotmail.com>
2017-11-03 19:48:03 -0200

    feat(DASHBOARD): add dashboard component

---
+++ src/components/Dashboard/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import Card from 'components/Card';
+import Panel from 'components/Panel';
+
+import cn from 'utils/cn';
+import media from 'utils/media';
+
+import './dashboard.css';
+
+const Dashboard = ({
+  children,
+  className,
+  noPad,
+  style
+}) => (
+  <Panel
+    column
+    fit
+    style={{
+      ...style
+    }}
+    className={
+      cn(
+        "dashboard__content",
+        (noPad || media.lessThan.tabletLandscape()) && 'dashboard__content--no-pad',
+        className
+      )
+    }
+    between={media.greaterThan.phone() ? 'l' : 'm'}
+  >
+    {children.map(child =>
+      <Card className="dashboard__card" noPad key={child.id}>
+        {child}
+      </Card>
+    )}
+  </Panel>
+);
+
+export default Dashboard;

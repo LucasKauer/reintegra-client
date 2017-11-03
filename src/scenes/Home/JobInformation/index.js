74903723b945332ce7d971ca2159960f96e69a63
Lucas de Freitas Kauer <lucas.kauerrs@hotmail.com>
2017-11-03 20:01:12 -0200

    feat(HOME): add dashboard with jobs informations to user's homescreen

---
+++ src/scenes/Home/JobInformation/index.js
@@ -0,0 +1,104 @@
+import React from 'react';
+
+import Panel from 'components/Panel';
+
+import './job-information.css';
+
+const JobInformationHeader = ({
+  title,
+  company,
+  city,
+  state,
+  country,
+  creationDate
+}) => (
+  <Panel column centered inset="l" between="xxs">
+    <h2>{title}</h2>
+    <small>
+      <span>{`${company} - ${city}/${state}, ${country}`}</span>
+    </small>
+    <small>
+      <span>{`Anunciada em ${(new Date(creationDate)).toLocaleString('pt-BR', { day: 'numeric', month: 'numeric', year: 'numeric' })}`}</span>
+    </small>
+  </Panel>
+);
+
+const JobInformationBody = ({
+  description,
+  experience,
+  industries,
+  employmentStatus,
+  jobFunctions
+}) => (
+  <Panel row>
+    <Panel flex="4" inset="l">
+      {description.split('\\n').map(paragraph =>
+        <p key={paragraph}>{paragraph}</p>
+      )}
+    </Panel>
+    <Panel flex="1" inset="l" column>
+      <Panel className="job-details" inset="xxs" between="xxs" column>
+        <h5 className="job-details__label job-details__label-title">Nível de experiência:</h5>
+        <p className="job-details__label job-details__label-body">{experience}</p>
+      </Panel>
+      <Panel className="job-details" inset="xxs" between="xxs" column>
+        <h5 className="job-details__label job-details__label-title">Setor:</h5>
+        <ul className="job-details__list">
+          {industries.map(i =>
+            <li className="job-details__list-item" key={i} >
+              <span key={`span-${i}`}>{i}</span>
+            </li>
+          )}
+        </ul>
+      </Panel>
+      <Panel className="job-details" inset="xxs" between="xxs" column>
+        <h5 className="job-details__label job-details__label-title">Tipo de emprego:</h5>
+        <p className="job-details__label job-details__label-body">{employmentStatus}</p>
+      </Panel>
+      <Panel className="job-details" inset="xxs" between="xxs" column>
+        <h5 className="job-details__label job-details__label-title">Funções de trabalho:</h5>
+        <ul className="job-details__list">
+          {jobFunctions.map(f =>
+            <li className="job-details__list-item" key={f} >
+              <span key={`span-${f}`}>{f}</span>
+            </li>
+          )}
+        </ul>
+      </Panel>
+    </Panel>
+  </Panel>
+);
+
+const JobInformation = ({
+  title,
+  company,
+  city,
+  state,
+  country,
+  creationDate,
+  description,
+  experience,
+  industries,
+  employmentStatus,
+  jobFunctions
+}) => (
+  <Panel>
+    <JobInformationHeader
+      title = {title}
+      company = {company}
+      city = {city}
+      state = {state}
+      country = {country}
+      creationDate = {creationDate}
+    />
+    <JobInformationBody
+      description = {description}
+      experience = {experience}
+      industries = {industries}
+      employmentStatus = {employmentStatus}
+      jobFunctions = {jobFunctions}
+    />
+  </Panel>
+);
+
+export default JobInformation;

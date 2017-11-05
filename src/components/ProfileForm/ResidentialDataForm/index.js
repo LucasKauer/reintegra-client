83cc7474166548e26a7e9974b512d064a2186567
Lucas de Freitas Kauer <lucas.kauerrs@hotmail.com>
2017-11-05 12:59:27 -0200

    feat(PROFILE): initial structure to residential data

---
+++ src/components/ProfileForm/ResidentialDataForm/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class ResidentialDataForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cep: '',
+      state: '',
+      city: '',
+      neighborhood: '',
+      address: '',
+      complement: '',
+      number: 0,
+    };
+  }
+
+  render() {
+    return (undefined);
+  };
+};
+
+export default ResidentialDataForm;

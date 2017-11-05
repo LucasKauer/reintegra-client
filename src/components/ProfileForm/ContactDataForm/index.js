55701285c714065ad66dc444040a5168a0a435c8
Lucas de Freitas Kauer <lucas.kauerrs@hotmail.com>
2017-11-05 12:59:38 -0200

    feat(PROFILE): initial structure to contact data

---
+++ src/components/ProfileForm/ContactDataForm/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class ContactDataForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      cellPhone: '',
+    };
+  }
+
+  render() {
+    return (undefined);
+  };
+};
+
+export default ContactDataForm;

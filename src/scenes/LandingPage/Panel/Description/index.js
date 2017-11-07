b7eae844bfbe47f4657565b90ae5bdeff706075a
Lucas de Freitas Kauer <lucas.kauerrs@hotmail.com>
2017-11-07 01:35:42 -0200

    feat(LANDING): description component

---
+++ src/scenes/LandingPage/Panel/Description/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Panel from 'components/Panel';
+
+const DescriptionPanel = ({ description }) => (
+  <Panel>
+    {description}
+  </Panel>
+);
+
+export default DescriptionPanel;

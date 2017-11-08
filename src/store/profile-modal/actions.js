998bde6cdd982354ed6ff1d72e2c9813a3436fd4
Lucas de Freitas Kauer <lucas.kauerrs@hotmail.com>
2017-11-07 23:04:44 -0200

    feat(PROFILE): reducer zzz

--- src/store/profile-modal/actions.js
+++ src/store/profile-modal/actions.js
@@ -3,11 +3,14 @@ import { createAction } from 'redux-actions';
 const setProfileModalState = createAction('SET_PROFILE_MODAL_STATE');
 
 const openProfileModal = () => setStatus({ profileSteps: 1, userProfile: { }});
-
 const closeProfileModal = () => setStatus({ profileSteps: 0, userProfile: { }});
 
 const nextStepProfileModal = ({ profileSteps, userProfile }, newDataToUserProfile) => setStatus({ profileSteps: profileSteps + 1, userProfile: Object.assign(userProfile, newDataToUserProfile) });
 const prevStepProfileModal = ({ profileSteps, userProfile }, newDataToUserProfile) => setStatus({ profileSteps: profileSteps - 1, userProfile: Object.assign(userProfile, newDataToUserProfile) });
+
+const updateAccount = user => (dispatch, _, api) =>
+  api.updateAccount(user)
+    .then(() => dispatch(closeProfileModal()));
 
 const setStatus = profile => dispatch => {
   dispatch(setProfileModalState(profile));
@@ -19,4 +22,5 @@ export {
   closeProfileModal,
   nextStepProfileModal,
   prevStepProfileModal,
+  updateAccount,
 };

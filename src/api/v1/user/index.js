d948afb9716b9195d9d8331c3dfeba260cbd3a19
Lucas de Freitas Kauer <lucas.kauer@cwi.com.br>
2017-11-10 15:33:42 -0200

    feat(USER): find user info by nickname

--- src/api/v1/user/index.js
+++ src/api/v1/user/index.js
@@ -3,4 +3,5 @@ import http from 'utils/http';
 export default {
   createAccount: user => http.post('user', user),
   updateUserInfo: user => http.put('user', user),
+  getUserInfoByNickname: nickname => http.get(`user/${nickname}`),
 };

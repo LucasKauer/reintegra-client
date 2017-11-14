48968db197fe43c1d66cca1966c0a070c3705f77
Lucas de Freitas Kauer <lucas.kauer@cwi.com.br>
2017-11-14 17:38:53 -0200

    refactor(JOB): improve reducer code

--- src/store/job/actions.js
+++ src/store/job/actions.js
@@ -7,19 +7,13 @@ const setJobsError = createAction('SET_JOBS_ERROR');
 const getJobs = () => (dispatch, _, api) => {
   api.getJobs()
     .then(({ dados }) => dispatch(setJobsState(dados)))
-    .catch(error => dispatch([
-      setJobsState([]),
-      setJobsError(error.message),
-    ]));
+    .catch(error => dispatch(setJobsError(error.message)));
 };
 
 const getJobsByTitle = title => (dispatch, _, api) => {
   api.getJobsByTitle(title)
     .then(({ dados }) => dispatch(setJobsState(dados)))
-    .catch(error => dispatch([
-      setJobsState([]),
-      setJobsError(error.message),
-    ]));
+    .catch(error => dispatch(setJobsError(error.message)));
 };
 
 export {
